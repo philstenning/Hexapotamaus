96d3a34b1b654c5ec120b259a11874d291959118
Phil Stenning <philstenning@hotmail.com>
2021-12-10 10:47:24 +0000

    clean up  classes

--- src/basic_movement.py
+++ src/basic_movement.py
@@ -1,5 +1,5 @@
 from typing import List
-import main_3
+
 import lewansoul_lx16a
 import time
 import serial
@@ -10,9 +10,8 @@ cf = currentframe()
 filename = getframeinfo(cf).filename
 
 
-
 class Motor(object):
-    def __init__(self, controller:lewansoul_lx16a.ServoController, id:int):
+    def __init__(self, controller: lewansoul_lx16a.ServoController, id: int):
         self.id = id
         self.controller = controller
 
@@ -28,22 +27,20 @@ class Motor(object):
             print("Could not move motor:", self.id,
                   filename, '@line:', cf.f_lineno)
 
-    def get_motor_settings(self) :
-        offset =int(controller.get_position_offset(self.id)) 
-        position = controller.get_position(self.id)
+    def get_motor_settings(self):
+        offset = int(self.controller.get_position_offset(self.id))
+        position = self.controller.get_position(self.id)
         print('motor_id:', self.id, 'offset:', offset, 'position:', position)
         return offset, position
 
     def reset_motor_offset(self):
-        controller.move(self.id, 500, 1000)# move to middle position so we don't break the servo/arm
-        print(controller.get_position(self.id))
-        print(controller.get_position_offset(self.id))
+        # move to middle position so we don't break the servo/arm
+        self.controller.move(self.id, 500, 1000)
+        print(self.controller.get_position(self.id))
+        print(self.controller.get_position_offset(self.id))
 
-        controller.set_position_offset(self.id, 0)
-        print(controller.get_position(self.id))
-
-
-
+        self.controller.set_position_offset(self.id, 0)
+        print(self.controller.get_position(self.id))
 
 
 class Leg(object):
@@ -56,15 +53,18 @@ class Leg(object):
 
         self.controller = controller
 
-    def set_position_by_angles(self, angles: List[int], time_seconds=1):
+    def set_position_by_angles(self, angles: List[int] = [90, 90, 90], time_seconds=1):
         """
         Set the position of this leg
         @param angles: [motor_1_angle, motor_2_angle, motor_3_angle]
         """
+        if angles.__len__() != 3:
+            print("angles must be a list of 3 integers")
+            return
         for i in range(3):
             self.motors[i].set_motor_position(angles[i], time_seconds)
 
-    def set_position(self, x, y, z, time_seconds=1):
+    def set_position(self, x: int, y: int, z: int, time_seconds: int = 1):
         """
         Set the position of a leg
         @param x: x-coordinate in cm  (out from center of leg joint)
@@ -75,19 +75,19 @@ class Leg(object):
         print('angles', angles)
         self.set_position_by_angles(angles, time_seconds)
 
-    def get_current_position(self) -> List[int]:
+    def get_motor_positions(self) -> List[int]:
         """
         @return: [motor_1_angle, motor_2_angle, motor_3_angle]
         this is between 0 and 1000 and not in degrees
         500 is 90 degrees
         """
-        angles = []
+        motor_positions: List[int] = []
         for i in range(3):
-            angles.append(controller.get_position(self.motors[i].id))
-        print('current angles for leg:', angles)
-        return angles
-    
-    def adjust_offsets(self, offsets:List[int]):
+            motor_positions.append(self.controller.get_position(self.motors[i].id))
+        print('current motor_positions for leg:', motor_positions)
+        return motor_positions
+
+    def adjust_offsets(self, offsets: List[int]):
         """
         Adjust the offsets of the motors
         @param offsets: [motor_1_offset, motor_2_offset, motor_3_offset]
@@ -95,15 +95,19 @@ class Leg(object):
         if offsets.__len__() != 3:
             print("offsets must be a list of 3 integers")
             return
-        before =[]
-        after = []
+        before = []
+        after: List[int] = []
         for i in range(3):
-            before.append(controller.get_position_offset(self.motors[i].id))
-            self.controller.set_position_offset(self.motors[i].id,int(offsets[i] + before[i]))
-            after.append(controller.get_position_offset(self.motors[i].id))
+            before.append(
+                self.controller.get_position_offset(self.motors[i].id))
+            self.controller.set_position_offset(
+                self.motors[i].id, int(offsets[i] + before[i]))
+            after.append(
+                self.controller.get_position_offset(self.motors[i].id))
         print('before:', before)
         print('after:', after)
-            
+        return after
+
 
 # SERIAL_PORT = 'COM4'
 # lewansoul_lx16a.init()
@@ -119,5 +123,3 @@ class Leg(object):
 
 # leg_1 = Leg(controller, M_1, M_2, M_3)
 # # leg_1.set_angle(90)
-
-
